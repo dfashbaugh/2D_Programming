d3e7ec089ba086b740f009695f92f8567e4a041f
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2016-10-27 00:27:35 -0400

    Basic setup for for loop creation

---
+++ ExamplePrograms/ForLoopExperiment/ForLoopExperiment.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "../../BaseInstructions/InstructionType.h"
+
+using namespace std;
+
+#define REG_FILE_SIZE 255
+
+void MakeAForLoop()
+{
+	int registers [REG_FILE_SIZE];
+	TwoDimensionalInstruction* instructions[10][10]; 
+}
+
+int main(void)
+{
+	MakeAForLoop();
+	return 0;
+}
