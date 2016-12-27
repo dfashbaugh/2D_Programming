4ccf3c531dda47b9f326b09af76c808504300be4
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2016-12-26 20:16:50 -0500

    Added a file stream to the parser

--- TextParser/ParserCPP/ProgramParser.cpp
+++ TextParser/ParserCPP/ProgramParser.cpp
@@ -1,8 +1,12 @@
 #include "ProgramParser.h"
+#include <fstream>
+
 
 ProgramParser::ProgramParser(std::string filePath)
 {
-
+	std::ifstream infile(filePath);
+	std::string line;
+	std::getline(infile, line);
 }
 
 ProgramParser::~ProgramParser()
