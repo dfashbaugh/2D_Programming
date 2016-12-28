56f7bd070c06b5b45c247e9057f10b24a1653a6e
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2016-12-28 01:16:16 -0500

    Added parsed instructions to internal instruction list

--- TextParser/ParserCPP/ProgramParser.cpp
+++ TextParser/ParserCPP/ProgramParser.cpp
@@ -19,14 +19,13 @@ void ProgramParser::LoadInstructionList(std::string filePath)
 	std::ifstream infile(filePath);
 	std::string line;
 
+	instrList.clear();
 	while(std::getline(infile, line))
 	{
-		
-
 		if(line.length() > 0)
 		{
 			TwoDimensionalInstructionData curData = GetInstructionData(line);
-			std::cout << curData.curX << " " << curData.curY << std::endl;
+			instrList.push_back(curData);
 		}
 	}
 	
